415ea546d783f3c90e760d1dac305d56dd7cdd7e
Shevi <s0556764078@gmail.com>
2025-07-07 18:53:44 +0300

    change URI

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.URI || 'https://smartbiz-server.onrender.com', // כתובת ה־API שלך
+  baseURL: process.env.URI || 'https://smartbiz-server.onrender.com/api/', // כתובת ה־API שלך
   withCredentials: true, // אם את משתמשת בקוקיז/אימות
 });
 
